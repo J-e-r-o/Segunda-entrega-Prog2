e2565b66cedf51d1845ccff77572fbc089bbbb5c
Marcos Barbieri <marcosbarbieri78@gmail.com>
2024-05-27 11:11:33 -0300

    Estructua

---
+++ src/ManipularCSV.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.
+
+public class ManipularCSV {
+    private BufferedReader Lector;
+    private String linea;
+    private String partes[] = null;
+
+    public void leerarchivo (String nombrearchivo){
+    try {
+
+    } catch() {
+
+    }
+
+    }
+}
